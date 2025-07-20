32dd63a554517ed952888366934d0dba8ed632e3
jererc <jererc@gmail.com>
2025-07-20 14:26:56 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='muteams',
-    version='2025.06.04.173027',
+    version='2025.07.20.142656',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/muteams',
